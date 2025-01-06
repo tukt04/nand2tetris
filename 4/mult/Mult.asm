293f653f3734ee1c1058115c6e4463dff8633bf1
Takuto Susuki <stukt04@gmail.com>
2025-01-07 04:32:30 +0900

    Add Mult.asm

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -8,3 +8,38 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+// R0 + R0 + ... + R0 (R1 times)
+@n
+M=0
+@R2
+M=0
+
+(LOOP)
+// if (n==R1) goto END
+@n
+D=M
+@R1
+D=M-D
+@END
+D;JEQ
+
+// R2 = R2 +R0
+@R2
+D=M
+@R0
+D=D+M
+@R2
+M=D
+
+// n = n + 1
+@n
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
